e65e517f12e843a70e5888d25b6074936261df62
amar13617 <amar13617@gmail.com>
2023-01-01 16:47:22 +0530

    Maximum Number commit

--- PyTest.py
+++ PyTest.py
@@ -1,3 +1,7 @@
-a = 10
-b = 5
-print(a+b)
+#Write a program to get the maximum number from a list.
+num = [23,55,7,8,9]
+max1 = num[0]
+for index in num:
+    if index > max1:
+        max1 = index
+print(max1)

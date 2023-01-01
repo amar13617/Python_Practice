1e2c78fc23373dd6eb996d101ed2f86505e7c885
amar13617 <amar13617@gmail.com>
2023-01-01 16:52:46 +0530

    Second Largest Number Commit

--- PyTest.py
+++ PyTest.py
@@ -4,4 +4,16 @@ max1 = num[0]
 for index in num:
     if index > max1:
         max1 = index
-print(max1)
+#print(max1)
+#Write a program to get the second maximum number from a list.
+num = [23,55,7,8,9]
+Largest_num = num[0]
+Second_num = num[0]
+for index in num:
+    if index > Largest_num:
+        Largest_num = index
+        for index in num:
+            if index > Second_num and index != Largest_num:
+                Second_num  = index
+print(Second_num)
+

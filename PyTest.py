6a444f8aa56e4ff07d4693268527bd7e717477cf
amar13617 <amar13617@gmail.com>
2023-01-01 19:17:51 +0530

    Remove Duplicate

--- PyTest.py
+++ PyTest.py
@@ -15,5 +15,14 @@ for index in num:
         for index in num:
             if index > Second_num and index != Largest_num:
                 Second_num  = index
-print(Second_num)
+#print(Second_num)
+
+#Write a program in Python to remove duplicate items from a list.
+num = [2,3,4,5,2,6,3,2]
+list1 = []
+for i in num:
+    if i not in list1:
+        list1.append(i)
+
+#print(list1)
 

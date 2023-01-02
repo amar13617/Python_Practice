7c174a0c0d0745cd6d376f5723735fdaf1d2ace1
amar13617 <amar13617@gmail.com>
2023-01-02 09:58:47 +0530

    maximum value from three number

--- PyTest.py
+++ PyTest.py
@@ -57,3 +57,13 @@ for i in dict1.values():
     if i > max:
         max = i
 #print(max)
+
+#Define a function in python that accepts 3 values and returns the maximum of three numbers.
+def maximum(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+#print(maximum(4,2,6))
